4aa2cfa022d225fef0481093959b7cf1e14bb6b5
Lukas Metlicka <lmetlicka@g.harvard.edu>
2016-05-02 20:27:52 -0400

    Generates patterns at a click.  still pretty slow.  shows vertices, algorithm for pattern may be off

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var canvas = document.getElementById("voroni");
 var ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
-ctx.imageSmoothingEnabled = false;
 /*var img = new Image();
 img.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
 img.onload = function() {
@@ -15,20 +14,16 @@ var color = [];
 
 
 $(document).ready(function($) {
-	
+	$("#think").hide('fast');
 	
 	$("#voroni").click(function(event) {
-		$("#think").show('fast', function() {
-			
-		});
-		vX.push(event.pageX);
-		vY.push(event.pageY);
+		$("#think").show('fast');
+		vX.push(event.pageX - $(this).offset().left);
+		vY.push(event.pageY - $(this).offset().top);
 		color.push(generateRandomColor());
 		generateVoroni(vX, vY, color);
-		$("#think").hide('fast', function() {
-			
-		});
-
+		generateVerticies(vX, vY);
+		$("#think").hide('fast');
 	});
 	/*for (i = 0; i < 100; i++){
 		vX.push(randomVert(0));
@@ -39,7 +34,11 @@ $(document).ready(function($) {
 	for (i = 0; i < vX.length; i++){
 		color.push(generateRandomColor());
 	}
-	generateVoroni(vX, vY, color);*/
+	generateVoroni(vX, vY, color);
+	CHANGES
+	*/
+
+
 });
 
 var generateVoroni = function(x, y, c){
@@ -48,20 +47,38 @@ var generateVoroni = function(x, y, c){
 	for(i = 0; i < canvas.height; i++){
 		for(j = 0; j < canvas.width; j++){
 			for (k = 0; k < x.length; k++){
-				if ( Math.hypot(j - x[k], i - y[k]) < Math.hypot(j - nearest[0], i - nearest[1]) ){
+				var a = Math.hypot(j - x[k], i - y[k]);
+				var b = Math.hypot(j - nearest[0], i - nearest[1])
+				if ( a < b ){
 					nearest = [x[k],y[k]];
 					color = c[k];
 				}
+				
 			}
 			paintPoint(j, i, color);
 			nearest = [canvas.width,canvas.height];
+			color = NaN;
 		}
 	}
 }
 
+function generateVerticies( x, y ){
+	for (i = 0; i < x.length; i++){
+		paintDot(x[i], y[i], "#000000")
+	}
+}
+
+/* POINT PAINTERS */
 function paintPoint(x, y, c){
 	ctx.fillStyle = c;
-	ctx.fillRect(x, y, 10, 10);
+	ctx.fillRect(x, y, 1, 1);
+}
+
+function paintDot(x, y){
+	ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
+    ctx.fillStyle = '#000000';
+    ctx.fill();
 }
 
 /* COLOR FUNCTIONS */
@@ -73,6 +90,7 @@ function componentToHex(c) {
 function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
+
 function generateRandomColor() {
 	var r = Math.floor(Math.random()* 255 - 0 + 1);
 	var g = Math.floor(Math.random()* 255 - 0 + 1);
