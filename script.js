046c21496c3fb9a336178328e2d5b8b55460bedf
Lukas Metlicka <lmetlicka@g.harvard.edu>
2016-05-15 12:27:14 -0400

    fixed mouse registration to accurately match canvas

--- script.js
+++ script.js
@@ -1,27 +1,84 @@
 var canvas = document.getElementById("voronoi");
 var ctx = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = 1000;
+canvas.height = 1000;
 
 var voronoi = new Voronoi();
 var image = new Image();
 
-var bbox = {xl: 0, xr: 500, yt: 0, yb: 500};
+var bbox = {xl: 0, xr: canvas.width, yt: 0, yb: canvas.height};
 var sites = [];
 var diagram = voronoi.compute(sites, bbox);
+
+var posX;
+var posY;
+var didDrag = false;
 
 $(document).ready(function($) {
 
 	/* 		LISTENERS 		*/
-	$("#voronoi").click(function(event) {
-		addVertex(event.pageX - $(this).offset().left, event.pageY - $(this).offset().top);
+	/*$("#voronoi").mousemove(function(event) {
+		posX = event.pageX - $(this).offset().left;
+		posY = event.pageY - $(this).offset().top;
+		console.log(posX + "," + posY);
+	});*/
+
+	$("#voronoi").mousedown(function(event) {
+		
+		for(i = 0; i < sites.length; i++){
+			var xMin = sites[i].x - 10;
+			var xMax = sites[i].x + 10;
+			var yMin = sites[i].y - 10;
+			var yMax = sites[i].y + 10;
+			if(posX > xMin && posX < xMax && posY > yMin && posY < yMax){
+				console.log(posX + " " + sites[i].x + "\t" + posY + " " + sites[i].y);
+				if(posX - sites[i].x >= 0){
+					sites[i].x += posX;
+				} else {
+					sites[i].x -= posX;
+				}
+				if(posY - sites[i].y >= 0){
+					sites[i].y += posY;
+				} else {
+					sites[i].y -= posY;
+				}
+				didDrag = true;
+				} else {
+				didDrag = false;
+			}
+		}
+			
+		});
+	
+
+	canvas.addEventListener("mousemove", function(evt){
+		var rect = canvas.getBoundingClientRect();
+		posX = (evt.pageX - rect.left) * (canvas.width / $("#voronoi").width()) ;
+		posY = (evt.pageY - rect.top) * (canvas.height / $("#voronoi").height());
+		console.log(posX + "," + posY);
+	});
+
+	$("#voronoi").mouseup(function(event) {
+		if( !didDrag ){
+			addVertex(posX, posY);
+			console.log(posX + " " + sites[sites.length - 1].x + "\t" + posY + " " + sites[sites.length - 1].y)
+			reloadCanvas();
+		}
+	});
+});
+	// Clear Button
+	$("#clear").click(function(event) {
+		sites = [];
 		reloadCanvas();
 	});
 
+
+	// Insert Point Button
 	$("#insertPoint_button").click(function(event) {
 		if (!isNaN($("#insertPoint_x").val()) && !isNaN($("#insertPoint_y").val())){
 			addVertex($("#insertPoint_x").val(), $("#insertPoint_y").val());
 			reloadCanvas();
+
 			$("#insertPoint_x").val("");
 			$("#insertPoint_y").val("");
 		} else {
@@ -30,7 +87,7 @@ $(document).ready(function($) {
 			$("#insertPoint_y").val("");
 		}
 	});
-
+	// Insert Point Keypress
 	$(document).keypress(function(event) {
 		if (event.keyCode == 13){
 			if (!isNaN($("#insertPoint_x").val()) && !isNaN($("#insertPoint_y").val())){
@@ -45,15 +102,22 @@ $(document).ready(function($) {
 		}
 		}
 	});
-
+	// File upload button
 	$('#file').change( function(event) {
     	image.src = URL.createObjectURL(event.target.files[0]);
     	image.onload = function(){
-    		reloadCanvas()
+    		canvas.width = image.width;
+    		canvas.height = image.height;
+    		bbox = {xl: 0, xr: canvas.width, yt: 0, yb: canvas.height}
+			ctx = canvas.getContext("2d");
+    		
+    		sites = [];
+
+    		reloadCanvas();
     	}
 	});
 
-});
+
 //redraws the canvas
 function reloadCanvas(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
